2e9ec0c02cc357245eff7693f18185c664a1992f
Lukas Herzberger <herzberger.lukas@gmail.com>
2021-03-09 14:09:52 +0100

    finish sample create-debug-utils-messenger

--- samples/create-debug-utils-messenger/create-debug-utils-messenger.lisp
+++ samples/create-debug-utils-messenger/create-debug-utils-messenger.lisp
@@ -2,15 +2,21 @@
 
 (in-package #:vk-samples/create-debug-utils-messenger)
 
+;; this function will actually log 
+(defun debug-utils-messenger (flags message-types callback-data user-data)
+  (format t "[~a] (~a): ~a; user-data: ~a~%"
+          flags message-types (vk:message callback-data) user-data))
+
 (cffi:defcallback debug-messenger-callback %vk:bool32 ((flags %vk:debug-utils-message-severity-flag-bits-ext)
                                                        (message-types %vk:debug-utils-message-type-flags-ext)
 					               (callback-data %vk:debug-utils-messenger-callback-data-ext)
 					               (user-data :pointer))
-  (debug-utils-messenger flags message-types callback-data user-data))
-
-(defun debug-utils-messenger (flags message-types callback-data user-data)
-  (format t "flags: ~a; message-types: ~a; callback-data: ~a; user-data: ~a~%"
-          flags message-types callback-data user-data)
+  (debug-utils-messenger flags
+                         message-types
+                         callback-data
+                         ;; the user data will be passed as a raw pointer in this example, the rest is translated automatically
+                         user-data)
+  ;; a debug callback MUST return VkFalse! (NIL will be translated to VkFalse automatically)
   nil)
 
 (defun create-debug-utils-messenger (&optional (app-name "create-debug-utils-messenger") (engine-name "vk"))
@@ -21,24 +27,17 @@
           "Could not find the ~a extension." vk:+ext-debug-utils-extension-name+)
   (with-instance (instance app-name engine-name nil (list vk:+ext-debug-utils-extension-name+
                                                           vk:+ext-debug-report-extension-name+))
-    
-    ;; todo: this should use the extension loader mechanism instead
-    ;; todo: document that order of parameters is not necessarily the same as in C API because some of them might be optional (e.g. instance in get-isntance-proc-addr)
-    (let ((pfn-create-debug-utils-messenger (vk:get-instance-proc-addr "vkCreateDebugUtilsMessengerEXT" instance))
-          (pfn-destroy-debug-utils-messenger (vk:get-instance-proc-addr "vkDestroyDebugUtilsMessengerEXT" instance)))
-      (assert pfn-create-debug-utils-messenger
-              ()
-              "Could not get function pointer for vkCreateDebugUtilsMessengerEXT!")
-      (assert pfn-destroy-debug-utils-messenger
-              ()
-              "Could not get function pointer for vkDestroyDebugUtilsMessengerEXT"))
-    
+    ;; supply the default extension loader with our instance
     (setf vk:*default-extension-loader* (vk:make-extension-loader :instance instance))
+    ;; set up the create info
     (let* ((create-info (make-instance 'vk:debug-utils-messenger-create-info-ext
                                        :message-type '(:general :performance :validation)
-                                       :message-severity '(:warning :error)
+                                       :message-severity '(:verbose :info :warning :error)
                                        :pfn-user-callback (cffi:get-callback 'debug-messenger-callback)
                                        :user-data (cffi:null-pointer)))
+           ;; create the debug utils messenger
            (messenger (vk:create-debug-utils-messenger-ext instance create-info)))
-      (vk:destroy-debug-utils-messenger-ext instance messenger))
-    (format t "created instance~%")))
+      ;; create and destroy a device to actually get some debug messages
+      (with-device (device instance))
+      ;; destroy the messenger - must be destroyed before the instance is destroyed!
+      (vk:destroy-debug-utils-messenger-ext instance messenger))))
